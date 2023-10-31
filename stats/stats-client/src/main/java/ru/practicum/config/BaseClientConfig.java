d1db393a9e37de46ef320154b1a6a5b2d0e1a2cc
Ivan Kotov <spicet@mail.ru>
2023-10-31 19:22:06 +0300

    #3

--- stats/stats-client/src/main/java/ru/practicum/config/BaseClientConfig.java
+++ stats/stats-client/src/main/java/ru/practicum/config/BaseClientConfig.java
@@ -1,13 +1,10 @@
 package ru.practicum.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.io.IOException;
 
 @Configuration
 public class BaseClientConfig {
