16bccb88ea281cfb547d54837a45bf90c7f8fe45
Ivan Kotov <spicet@mail.ru>
2023-10-31 19:24:51 +0300

    #3

--- stats/stats-client/src/main/java/ru/practicum/config/BaseClientConfig.java
+++ stats/stats-client/src/main/java/ru/practicum/config/BaseClientConfig.java
@@ -1,24 +1,11 @@
 package ru.practicum.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class BaseClientConfig {
-    private static ApplicationContext applicationContext;
-
-    @Autowired
-    public BaseClientConfig(ApplicationContext context) {
-        applicationContext = context;
-    }
-
-    public static ApplicationContext getApplicationContext() {
-        return applicationContext;
-    }
-
     @Bean
     public WebClient webClient() {
         return WebClient.create("http://localhost:9090");
