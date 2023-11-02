bd6d37727ef15a322a83372333432b2905713380
Ivan Kotov <spicet@mail.ru>
2023-11-02 09:26:56 +0300

    #5

--- stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHit.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHit.java
@@ -1,12 +1,10 @@
 package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
-@Data
 public class EndpointHit {
     @NotBlank
     String app;
@@ -16,4 +14,11 @@ public class EndpointHit {
     String ip;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime timestamp;
+
+    public EndpointHit(String app, String uri, String ip, LocalDateTime timestamp) {
+        this.app = app;
+        this.uri = uri;
+        this.ip = ip;
+        this.timestamp = timestamp;
+    }
 }
--- stats/stats-dto/src/main/java/ru/practicum/dto/ViewStats.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/ViewStats.java
@@ -1,8 +1,5 @@
 package ru.practicum.dto;
 
-import lombok.Data;
-
-@Data
 public class ViewStats {
     String app;
     String uri;
@@ -11,6 +8,30 @@ public class ViewStats {
     public ViewStats(String app, String uri, Long hits) {
         this.app = app;
         this.uri = uri;
+        this.hits = hits;
+    }
+
+    public String getApp() {
+        return app;
+    }
+
+    public void setApp(String app) {
+        this.app = app;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public Long getHits() {
+        return hits;
+    }
+
+    public void setHits(Long hits) {
         this.hits = hits;
     }
 }
